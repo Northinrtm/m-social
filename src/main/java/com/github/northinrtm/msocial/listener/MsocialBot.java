d854aed3fd724a89783ce87a54147cf3c2c7ed69
northinrtm <northinrtm@gmail.com>
2024-01-04 19:33:20 +0300

    now the bot is responding to the user

--- src/main/java/com/github/northinrtm/msocial/listener/MsocialBot.java
+++ src/main/java/com/github/northinrtm/msocial/listener/MsocialBot.java
@@ -7,7 +7,9 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 import org.telegram.telegrambots.bots.DefaultAbsSender;
 import org.telegram.telegrambots.bots.DefaultBotOptions;
+import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
 import org.telegram.telegrambots.meta.api.objects.Update;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 import org.telegram.telegrambots.meta.exceptions.TelegramApiRequestException;
 import org.telegram.telegrambots.meta.generics.LongPollingBot;
 
@@ -48,10 +50,23 @@ public class MsocialBot extends DefaultAbsSender implements LongPollingBot {
             } else {
                 response = "Вот тебе такой ответ на твой запрос: " + request;
             }
-            messageService.createMessage(request,response, userChatId);
+
+
+            createAndSendMessage(request, response, userChatId);
         }
     }
 
+    public void createAndSendMessage(String request, String response, long userChatId) {
+        try {
+            execute(SendMessage.builder()
+                    .chatId(userChatId)
+                    .text(response)
+                    .build());
+        } catch (TelegramApiException e) {
+            throw new RuntimeException(e);
+        }
+        messageService.createMessage(request, response, userChatId);
+    }
 
     @Override
     public void clearWebhook() throws TelegramApiRequestException {
